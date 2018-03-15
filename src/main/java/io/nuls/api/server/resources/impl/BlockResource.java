713903be6b0d2a86b92076a468aec8676c0eda0e
MIMIEYES <pierreluohao@gmail.com>
2018-03-15 20:04:39 +0800

    add post test

--- src/main/java/io/nuls/api/server/resources/impl/BlockResource.java
+++ src/main/java/io/nuls/api/server/resources/impl/BlockResource.java
@@ -23,16 +23,14 @@
  */
 package io.nuls.api.server.resources.impl;
 
+import io.nuls.api.entity.Alias;
 import io.nuls.api.entity.RpcClientResult;
 import io.nuls.api.utils.RestFulUtils;
 import io.nuls.api.utils.StringUtils;
 import io.nuls.api.utils.log.Log;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -134,6 +132,21 @@ public class BlockResource {
         }
         try {
             result = RestFulUtils.getInstance().get("/block/header/hash/"+hash, null);
+        } catch (Exception e) {
+            result = RpcClientResult.getFailed();
+            Log.error(e);
+        }
+        return result;
+    }
+
+    @POST
+    @Path("/test")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RpcClientResult testMethod(Alias alias) {
+        RpcClientResult result;
+        try {
+            result = RpcClientResult.getSuccess();
+            result.setData(alias);
         } catch (Exception e) {
             result = RpcClientResult.getFailed();
             Log.error(e);
