94faca3e18b6fffeb7dfa56a16c6b0726b43e9e0
MIMIEYES <pierreluohao@gmail.com>
2018-03-18 14:51:15 +0800

    fix some bugs about shell scripts

--- startup.sh
+++ startup.sh
@@ -4,10 +4,26 @@ mvn clean -DskipTests=true package
 
 cd target
 
-JAVA_OPTS="-Xms3072m -Xmx3072m -Xss128k -XX:NewRatio=1 -XX:SurvivorRatio=9 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=5 -XX:MaxTenuringThreshold=15"
+if [ ! -d "logs" ]; then
+    mkdir logs
+fi
+status=$?
+if [ $status -ne 0 ]; then
+    echo "start the Server failed."
+    exit $status
+fi
 
-CLASS_PATH="./;./API-Server-1.0-SNAPSHOT.jar"
+JAVA_OPTS="-Xms3072m -Xmx3072m -Xss160k -XX:NewRatio=1 -XX:SurvivorRatio=9 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=5 -XX:MaxTenuringThreshold=15"
 
-nohup java -Dfile.encoding=UTF-8 $JAVA_OPTS -classpath $CLASS_PATH io.nuls.main.Main >nohup.log 2>&1 &
+CLASS_PATH="./:./API-Server-1.0-SNAPSHOT.jar"
 
+echo "now starting the API-Server..."
+
+nohup java -Dfile.encoding=UTF-8 $JAVA_OPTS -classpath $CLASS_PATH io.nuls.main.Main >./logs/starting.log 2>&1 &
+
+if [ $? -ne 0 ]; then
+    echo "start failed."
+else
+    echo "start completed."
+fi
 exit 0
