cf8335bdb1792dfb87ff2c1d7f216869dfd2b349
MIMIEYES <pierreluohao@gmail.com>
2018-03-19 20:54:05 +0800

    fix some bug about input.close()

--- src/main/java/io/nuls/api/utils/PropertiesUtils.java
+++ src/main/java/io/nuls/api/utils/PropertiesUtils.java
@@ -2,16 +2,29 @@ package io.nuls.api.utils;
 
 import io.nuls.api.utils.log.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesUtils {
     private static Properties properties=new Properties();
 
     static{
+        InputStream in = null;
         try {
-            properties.load(PropertiesUtils.class.getClassLoader().getResourceAsStream("sys.properties"));
+            in = PropertiesUtils.class.getClassLoader().getResourceAsStream("sys.properties");
+            properties.load(in);
         } catch (Exception e) {
             Log.error(e);
+        } finally {
+            if(in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    //skip
+                }
+            }
+
         }
     }
 
