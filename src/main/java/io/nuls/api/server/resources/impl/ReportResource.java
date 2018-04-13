7739da538053b38b6e8ec56793e3c3557985e7a8
MIMIEYES <pierreluohao@gmail.com>
2018-04-13 14:33:53 +0800

    fix npe bug

--- src/main/java/io/nuls/api/server/resources/impl/ReportResource.java
+++ src/main/java/io/nuls/api/server/resources/impl/ReportResource.java
@@ -145,15 +145,19 @@ public class ReportResource {
             try {
                 RpcClientResult status = RestFulUtils.getInstance().get("/consensus/agent/status", null);
                 Map<String, String> statusMap = (Map<String, String>) status.getData();
-                if(statusMap != null) {
-                    //Map<String, String> statusMap = statusList.stream() .collect(Collectors.toMap(map -> map.get("address"), map -> map.get("consensusStatus")));
-                    list.stream().forEach(minedTop -> {
-                        String _status = statusMap.get(minedTop.getConsensusAddress());
+
+                //Map<String, String> statusMap = statusList.stream().collect(Collectors.toMap(map -> map.get("address"), map -> map.get("consensusStatus")));
+
+                list.stream().forEach(minedTop -> {
+                    String _status = "0";
+                    if(statusMap != null) {
+                        _status = statusMap.get(minedTop.getConsensusAddress());
                         if(_status == null)
                             _status = "0";
-                        minedTop.setConsensusStatus(_status);
-                    });
-                }
+                    }
+                    minedTop.setConsensusStatus(_status);
+                });
+
             } catch (Exception e) {
                 Log.warn("can not get consensus status list.", e);
                 // skip
