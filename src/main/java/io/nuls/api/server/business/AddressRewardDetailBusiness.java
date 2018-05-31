ee48bbb8113d1f8c7323da46a9ea0d940be23c02
zengsj <906606200@qq.com>
2018-05-31 15:38:41 +0800

    api接口调整

--- src/main/java/io/nuls/api/server/business/AddressRewardDetailBusiness.java
+++ src/main/java/io/nuls/api/server/business/AddressRewardDetailBusiness.java
@@ -1,13 +1,14 @@
 package io.nuls.api.server.business;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.nuls.api.entity.AddressRewardDetail;
 import io.nuls.api.server.dao.mapper.AddressRewardDetailMapper;
 import io.nuls.api.server.dao.util.SearchOperator;
 import io.nuls.api.server.dao.util.Searchable;
+import io.nuls.api.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Description: 账户奖励
@@ -25,10 +26,51 @@ public class AddressRewardDetailBusiness {
      * @param address 奖励的账户地址
      * @return
      */
-    public List<AddressRewardDetail> getList(String address) {
+    public PageInfo<AddressRewardDetail> getList(String address,int pageNumber, int pageSize) {
+        PageHelper.startPage(pageNumber, pageSize);
         Searchable searchable = new Searchable();
         searchable.addCondition("address", SearchOperator.eq, address);
-        return addressRewardDetailMapper.selectList(searchable);
+        PageInfo<AddressRewardDetail> page = new PageInfo<>(addressRewardDetailMapper.selectList(searchable));
+        return page;
+    }
+
+    /**
+     * 新增奖励
+     * @param entity 需要新增的实体，除了时间之外，其他字段都需要值，否则会保存失败
+     * @return 新增结果 1成功，其他失败
+     */
+    public int add(AddressRewardDetail entity){
+        entity.setTime(System.currentTimeMillis());
+        return addressRewardDetailMapper.insert(entity);
+    }
+
+    /**
+     * 根据id查询
+     * @param id
+     * @return
+     */
+    public AddressRewardDetail getDetail(Long id){
+        return addressRewardDetailMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 根据主键删除数据
+     * @param id
+     * @return
+     */
+    public int deleteById(Long id){
+        return addressRewardDetailMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据高度删除某高度所有数据
+     * @param height
+     * @return
+     */
+    public int deleteByHeight(Long height){
+        Searchable searchable = new Searchable();
+        searchable.addCondition("block_height", SearchOperator.eq, height);
+        return addressRewardDetailMapper.deleteBySearchable(searchable);
     }
 
 }
--- src/main/java/io/nuls/api/server/business/AgentNodeBusiness.java
+++ src/main/java/io/nuls/api/server/business/AgentNodeBusiness.java
@@ -1,5 +1,8 @@
 package io.nuls.api.server.business;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import io.nuls.api.constant.EntityConstant;
 import io.nuls.api.entity.AgentNode;
 import io.nuls.api.server.dao.mapper.AgentNodeMapper;
 import io.nuls.api.server.dao.util.SearchOperator;
@@ -7,8 +10,6 @@ import io.nuls.api.server.dao.util.Searchable;
 import io.nuls.api.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Description: 节点
@@ -23,15 +24,17 @@ public class AgentNodeBusiness{
 
     /**
      * 获取列表
-     * @param agentAddress 创建地址
+     * @param agentName 节点名称 (搜索用)
      * @return
      */
-    public List<AgentNode> getList(String agentAddress) {
+    public PageInfo<AgentNode> getList(String agentName, int pageNumber, int pageSize) {
+        PageHelper.startPage(pageNumber, pageSize);
         Searchable searchable = new Searchable();
-        if(StringUtils.isNotBlank(agentAddress)){
-            searchable.addCondition("agent_address", SearchOperator.eq, agentAddress);
+        if(StringUtils.isNotBlank(agentName)){
+            searchable.addCondition("agent_name", SearchOperator.like, agentName);
         }
-        return agentNodeMapper.selectList(searchable);
+        PageInfo<AgentNode> page = new PageInfo<>(agentNodeMapper.selectList(searchable));
+        return page;
     }
 
     /**
@@ -44,5 +47,48 @@ public class AgentNodeBusiness{
         searchable.addCondition("agent_address", SearchOperator.eq, agentAddress);
         return agentNodeMapper.selectBySearchable(searchable);
     }
+
+    /**
+     * 根据id查询
+     * @param txHash 创建地址
+     * @return
+     */
+    public AgentNode getDetail(String txHash) {
+        return agentNodeMapper.selectByPrimaryKey(txHash);
+    }
+
+    /**
+     * 根据id查询
+     * @param entity 创建节点的实体，除了status和time其他的都需要传
+     * @return
+     */
+    public int save(AgentNode entity) {
+        entity.setCreateTime(System.currentTimeMillis());
+        entity.setStatus(EntityConstant.CONSENSUS_STATUS_WAIT);
+        return agentNodeMapper.insert(entity);
+    }
+
+    /**
+     * 根据主键删除节点
+     * @param txHash  主键 txhash
+     * @return
+     */
+    public int deleteById(String txHash){
+        return agentNodeMapper.deleteByPrimaryKey(txHash);
+    }
+
+    /**
+     * 根据高度删除某高度所有节点
+     * @param height  高度
+     * @return
+     */
+    public int deleteByHeight(Long height){
+        Searchable searchable = new Searchable();
+        searchable.addCondition("block_height", SearchOperator.eq, height);
+        return agentNodeMapper.deleteBySearchable(searchable);
+    }
+
+
+
 
 }
