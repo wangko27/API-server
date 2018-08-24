322b6dd2207201a52705edd3b492cc7379fcdac8
zengsj <906606200@qq.com>
2018-08-24 17:39:27 +0800

    utxo-update

--- src/main/java/io/nuls/api/context/UtxoContext.java
+++ src/main/java/io/nuls/api/context/UtxoContext.java
@@ -21,25 +21,19 @@ public class UtxoContext {
     public static void put(String address, String key) {
         Set<String> list = get(address);
         list.add(key);
-        //重置缓存，有效期为一小时
-        EhcacheUtil.getInstance().putWithTime(Constant.UTXO_CACHE_NAME, address, list,Constant.SECONDS_TIME_HOUR_SECOND);
-        //重置leveldb
-        addressHashIndexLevelDbService.insert(new AddressHashIndex(address, list));
+        put(new AddressHashIndex(address, list));
     }
 
-    public static void put(String address, Set<String> list){
+    public static void put(AddressHashIndex addressHashIndex){
         //重置缓存
-        EhcacheUtil.getInstance().putWithTime(Constant.UTXO_CACHE_NAME, address, list,Constant.SECONDS_TIME_HOUR_SECOND);
+        EhcacheUtil.getInstance().putWithTime(Constant.UTXO_CACHE_NAME, addressHashIndex.getAddress(), addressHashIndex.getHashIndexSet(),Constant.SECONDS_TIME_HOUR_SECOND);
         //重置leveldb
-        addressHashIndexLevelDbService.insert(new AddressHashIndex(address, list));
+        addressHashIndexLevelDbService.insert(addressHashIndex);
     }
 
     public static void putMap(Map<String,AddressHashIndex> attrMapList){
         for(AddressHashIndex addressHashIndex:attrMapList.values()){
-            //重置缓存
-            EhcacheUtil.getInstance().putWithTime(Constant.UTXO_CACHE_NAME, addressHashIndex.getAddress(), addressHashIndex.getHashIndexSet(),Constant.SECONDS_TIME_HOUR_SECOND);
-            //重置leveldb
-            addressHashIndexLevelDbService.insert(addressHashIndex);
+            put(addressHashIndex);
         }
     }
 
@@ -54,10 +48,13 @@ public class UtxoContext {
                     setList = new HashSet<>();
                 }
             }else{
+                addressHashIndex = new AddressHashIndex();
                 setList = new HashSet<>();
             }
             //如果不存在，则直接重新放入缓存
-            put(address,setList);
+            addressHashIndex.setAddress(address);
+            addressHashIndex.setHashIndexSet(setList);
+            put(addressHashIndex);
         }
         return setList;
     }
